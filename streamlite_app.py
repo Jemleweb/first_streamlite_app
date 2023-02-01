9444dd2a380c8af028bef1698171aed004b22b41
Jemleweb <jeremie.delobel@jemleweb.fr>
2023-01-31 21:36:13 -0500

    Update streamlite_app.py
    change favorite

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,6 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breakfast menu')
-streamlit.text('Salade de bettrave')
+  
+streamlit.header('Breakfast favorites')
+streamlit.text(' Salade de bettrave')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('🥑🍞 Avocado Toast')
